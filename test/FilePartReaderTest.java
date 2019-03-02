2207745745e9d8fefb776cf274fe2476f98e9a68
Los4U <Backup_2018>
2019-03-02 19:52:00 +0100

    start creating first test for FilePartReader class

---
+++ test/FilePartReaderTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilePartReaderTest {
+
+    @Test
+    void setup() {
+
+
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void read() {
+    }
+}
